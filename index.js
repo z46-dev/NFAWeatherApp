2ef408460ecd95623d4ef6eab67639ffe4709ad0
z46-dev <45317380+z46-dev@users.noreply.github.com>
2024-02-26 19:50:04 -0500

    Refactor code and add new loop function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { SerialPort } from "serialport";
 import { parse } from "./lib/parser.js";
 
+let loopListener = undefined,
+    barDataListener = undefined;
+
 const port = new SerialPort({
     path: "/dev/ttyUSB0",
     baudRate: 19200
-})
+});
 
 port.on("open", function onOpen() {
     console.log("Port is open");
@@ -20,9 +23,35 @@ port.on("open", function onOpen() {
 
 port.on("data", function onData(data) {
     console.log(data.length, "bytes of data received");
-    try {
-        console.log(parse(data));
-    } catch (err) {
-        console.error("Error:", err);
+    
+    if (loopListener !== undefined) {
+        loopListener();
+        loopListener = undefined;
     }
+});
+
+function loop(timeout = 1000) {
+    return new Promise((resolve, reject) => {
+        if (loopListener !== undefined) {
+            reject(new Error("Another LOOP is already running"));
+            return;
+        }
+
+        setTimeout(reject, timeout, new Error("LOOP Timeout"));
+
+        port.write("LOOP 1\n", function onWrite(err) {
+            if (err) {
+                return reject(err);
+            }
+
+            loopListener = resolve;
+        });
+    });
+}
+
+loop().then(data => {
+    console.log("LOOP 1 is done");
+    console.log(parse(data));
+}).catch((err) => {
+    console.error("Error:", err);
 });
